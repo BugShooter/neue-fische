30703a040520b9a2c92fd432521cb672095aefbc
Alexey Muravyov <muravyov.alexey@gmail.com>
2025-07-02 11:19:00 +0000

    test: add additional test cases for isPalindrome function

--- day-006/hands-on-tdd/src/utils/isPalindrome.test.ts
+++ day-006/hands-on-tdd/src/utils/isPalindrome.test.ts
@@ -22,4 +22,13 @@ describe("isPalindrome(word:string):string", () => {
     test("'Madam' → false", () => {
         expect(isPalindrome('Madam')).toBe(false);
     });
+    test("'12321' → true", () => {
+        expect(isPalindrome('12321')).toBe(true);
+    });
+    test("'12345' → false", () => {
+        expect(isPalindrome('12345')).toBe(false);
+    });
+    test("'!@#@!' → true", () => {
+        expect(isPalindrome('!@#@!')).toBe(true);
+    });
 });
